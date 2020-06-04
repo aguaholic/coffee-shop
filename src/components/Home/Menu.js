9af850c7052b2ae2718ea31153e6af210db9abad
aguaholic <joyterra@gmail.com>
2020-06-04 11:00:03 +0200

    Filter by category

--- src/components/Home/Menu.js
+++ src/components/Home/Menu.js
@@ -2,9 +2,32 @@ import React, { useState } from 'react'
 import Title from '../Globals/Title'
 import Img from 'gatsby-image'
 
+const getCategories = items => {
+    let tempItems = items.map(items => {
+        return items.node.category
+    })
+    let tempCategories = new Set(tempItems)
+    let categories = Array.from(tempCategories)
+    categories = ['all', ...categories]
+    return categories
+}
+
 const Menu = ({ items }) => {
     const [stateItems, setItems] = useState(items.edges)
     const [coffeeItems, setCoffeeItems] = useState(items.edges)
+    const [categories, setCategories] = useState(getCategories(items.edges))
+
+    const handleItems = category => {
+        let tempItems = [...stateItems]
+        if (category === 'all') {
+            setCoffeeItems(tempItems)
+        } else {
+            let items = tempItems.filter(
+                ({ node }) => node.category === category
+            )
+            setCoffeeItems(items)
+        }
+    }
 
     return (
         <>
@@ -12,6 +35,24 @@ const Menu = ({ items }) => {
                 <section className="menu py-5">
                     <div className="container">
                         <Title title="Best of our menu" />
+                        <div className="row mb-5">
+                            <div className="col-10 mx-auto text-center">
+                                {categories.map((category, index) => {
+                                    return (
+                                        <button
+                                            className="btn btn-yellow text-capitalize m-3"
+                                            onClick={() => {
+                                                handleItems(category)
+                                            }}
+                                            key={index}
+                                            type="button"
+                                        >
+                                            {category}
+                                        </button>
+                                    )
+                                })}
+                            </div>
+                        </div>
                         <div className="row">
                             {coffeeItems.map(({ node }) => {
                                 return (
@@ -25,10 +66,10 @@ const Menu = ({ items }) => {
                                         <div className="flex-grow-1 px-3">
                                             <div className="d-flex justify-content-between">
                                                 <h6 className="mb-0">
-                                                    {node.title}
+                                                    <small>{node.title}</small>
                                                 </h6>
-                                                <h6 className="mb-0">
-                                                    ${node.price}
+                                                <h6 className="mb-0 text-yellow">
+                                                    <small>${node.price}</small>
                                                 </h6>
                                             </div>
                                             <p className="text-muted">
